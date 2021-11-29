43d26b1abe7099407a2f17d04f9ec2e4ba0bd729
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-11-29 07:30:22 +0700

    ok

--- app.py
+++ app.py
@@ -50,9 +50,9 @@ def index():
 def sendmessage():
     message = request.json['message']
     response_message = bot.get_response(message)
-    print(response_message)
-    return make_response(jsonify({'success': True,
-                                  'response_message': response_message}), 200)
+    print("response_message", response_message)
+    # return make_response(jsonify({'success': True,
+    #                               'response_message': response_message}), 200)
     # return json.dumps({'success': True,
     #                    'response_message': response_message})
 
