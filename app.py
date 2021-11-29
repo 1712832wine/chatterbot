77b3052690c0e908b72d7315dfa390c3d0265a6a
NGUYỄN HỬU TRÍ <nguyenhuutri31081999nht@gmail.com>
2021-11-29 07:26:53 +0700

    ok

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
 import chatterbot
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import jsonify, make_response
 import json
-
 # text
 app = Flask(__name__)
 
@@ -31,19 +32,29 @@ for file in os.listdir(path):
     file_path = './content/english/' + file
     trainer.train(file_path)
 
-print('Type something to begin...')
-
 # The following loop will execute each time the user enters input
+# incoming_msg = request.values.get('message').lower()
+#     print("incoming_msg", incoming_msg)
+#
+#     resp_msg = resp.text
+#     json_resp = {"success": true, "response_message": resp_msg}
+#     return jsonify({'task': task}), 201
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 
 @app.route('/api/message/send', methods=['POST'])
-def bot():
-    incoming_msg = request.values.get('message').lower()
-    resp = bot.get_response(incoming_msg)
-    resp_msg = resp.text
-    json_resp = {"success": true, "response_message": resp_msg}
-    return json.dumps(json_resp)
+def sendmessage():
+    message = request.json['message']
+    response_message = bot.get_response(message)
+    return make_response(jsonify({'success': True,
+                                  'response_message': response_message}), 200)
+    # return json.dumps({'success': True,
+    #                    'response_message': response_message})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
