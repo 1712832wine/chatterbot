8fff037dfd14256a6e81eef86c5e4f4acc8f2d2f
1712832wine <nguyenhuutri31081999nht@gmail.com>
2021-12-07 09:32:48 +0700

    ok

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
+# This file is using python 3.9.8
+
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 
-import os
-import chatterbot
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-# text
 app = Flask(__name__)
 CORS(app)
 
-bot = ChatBot(
-    "English Bot",
-    logic_adapters=[
-        "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.BestMatch",
-    ],
-)
-trainer = ChatterBotCorpusTrainer(bot)
-path = './content/english'
-for file in os.listdir(path):
-    file_path = './content/english/' + file
-    trainer.train(file_path)
+conversation = [
+    "Xin chào",
+    "Chào bạn",
+    "Bạn đang làm gì đó?",
+    "Tôi đang học",
+    "Nghe tuyệt đó",
+    "Cảm ơn",
+    "Không có chi"
+]
+# Create chatbot
+chatbot = ChatBot("Ron Obvious",
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3')
+# train
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
 
 
 @app.route('/')
@@ -32,9 +35,9 @@ def index():
 @app.route('/api/messages/send', methods=['POST'])
 def sendmessage():
     message = request.json['message']
-    response_message = bot.get_response(message)
-    return jsonify({"success": True,
-                    "response_message": str(response_message)})
+    response_message = chatbot.get_response(message)
+    print("response_message",response_message)
+    return jsonify({"success": True, "response_message": str(response_message)})
 
 
 if __name__ == '__main__':
