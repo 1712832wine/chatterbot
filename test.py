37594fd9103bc8115c8fe1bcee97407a91ad59b2
1712832wine <nguyenhuutri31081999nht@gmail.com>
2022-03-14 17:01:10 +0700

    ok

--- test.py
+++ test.py
@@ -9,9 +9,11 @@ def get_response(message):
     response = chatbot.get_response(message)
     if response.confidence >= 0.75:
         result = response.text
+        type = 'data'
     else:
         result = googleSearch(message)
-    return result
+        type = 'google'
+    return result, type
 
 
 if __name__ == "__main__":
@@ -22,6 +24,7 @@ if __name__ == "__main__":
         total_score = 0
         total_len = 0
         list_test = os.listdir('./data/test')
+        google = 0
         for file in list_test:
             with open('./data/test/'+file, encoding="utf-8") as f:
                 test = f.read()
@@ -45,11 +48,13 @@ if __name__ == "__main__":
 
             for index in range(0, len(test_questions)):
                 print(index + 1, '/', len(test_questions), 'of', file)
-                predict_text = get_response(test_questions[index])
+                predict_text, type = get_response(test_questions[index])
 
                 if compare(predict_text, test_answers[index]):
                     score += 1
                 else:
+                    if type == 'google':
+                        google += 1
                     # WRONG CASES
                     fw.write('{}---------------------\n'.format(type))
                     fw.write('TEST QUESTION: {}\n'.format(
@@ -69,5 +74,5 @@ if __name__ == "__main__":
         # finish all
         time_end = time.time()
         total_time = time_end - time_start
-        fw.write('total_score: {} || len: {} || score: {} \n||time: {}\n'.format(
-            total_score, total_len, total_score/total_len, total_time))
+        fw.write('total_score: {} || google:{} || len: {} || score: {} \n||time: {}\n'.format(
+            total_score, google, total_len, total_score/total_len, total_time))
